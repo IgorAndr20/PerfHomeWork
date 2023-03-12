bf2c04e81a62b256e57bc34ec49452edaa914859
Igor <test@gmail.com>
2023-03-12 23:46:21 +0400

    Урок 66

---
+++ src/lesson66/task1/megabankcorp/records/Account.java
@@ -0,0 +1,21 @@
+package lesson66.task1.megabankcorp.records;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private BigDecimal sum;
+    private long acc;
+    private long number;
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public long getAcc() {
+        return acc;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+}
---
+++ src/lesson66/task1/megabankcorp/system/Database.java
@@ -0,0 +1,21 @@
+package lesson66.task1.megabankcorp.system;
+
+import lesson66.task1.megabankcorp.records.*;
+
+import java.math.BigDecimal;
+
+public class Database {
+    public void deposit(BigDecimal sum, Account num) {
+    }
+
+    public void withdraw(BigDecimal sum, Account num) {
+    }
+
+    protected void getAmount(Account num) {
+    }
+
+    void transfer(Account num, Account num2) {
+    }
+}
+
+
