db326deeeb827fc070c0c894d804cd75ac552b7f
Igor <test@gmail.com>
2023-03-16 00:08:24 +0400

    Урок 54 доработки

--- src/lesson54/Task1.java
+++ src/lesson54/Task1.java
@@ -7,6 +7,7 @@ public class Task1 {
     public static void main(String[] args) {
         String ch = args[0];
         String strcut = str.replaceAll("\\s","" );
-        System.out.println(strcut.indexOf(ch));
+        if (!strcut.contains(ch)) System.out.println("Символа нет в строке ");
+        else System.out.println(strcut.indexOf(ch));
     }
 }
--- src/lesson54/Task2.java
+++ src/lesson54/Task2.java
@@ -3,7 +3,10 @@ package lesson54;
 public class Task2 {
     public static void main(String[] args) {
         String path = args[0];
-        if (path.endsWith("\"") | path.endsWith("/")) {
+        for (int i = 1; i < args.length; i++) {
+            path = path + " " + args[i];
+        }
+        if ((path.endsWith("\\") | path.startsWith("\\")) | path.endsWith("/") | path.contains("\\\\") | path.contains("//")) {
             System.out.println("Введен некорректный путь до файла.");
         } else {
             if (path.contains("" + "\\")) {
